1216ea506f09e5f4575c341e80257bce4d3acaaa
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-03-27 02:12:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Remove the main element with id 'main'
+const main = document.querySelector('#main');
+main.remove();
+
+// Create a new h1 element with id 'victory' and text content
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new h1 element to the body of the document
+document.body.appendChild(newHeader);
